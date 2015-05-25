523746d86337610e32be5c60f2dfbbf4a05e5da1
innamuri.lakshmi@gmail.com <lakshmikollapudi@Lakshmis-MacBook-Air.local>
2015-05-25 00:35:27 -0400

    login view is set

--- MailClientIOS/ViewController.h
+++ MailClientIOS/ViewController.h
@@ -10,6 +10,9 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UITextField *id_user;
+@property (weak, nonatomic) IBOutlet UITextField *pwd_user;
+- (IBAction)retrieve;
 
 @end
 
--- MailClientIOS/ViewController.m
+++ MailClientIOS/ViewController.m
@@ -10,9 +10,13 @@
 
 @interface ViewController ()
 
+
 @end
 
 @implementation ViewController
+
+@synthesize id_user;
+@synthesize pwd_user;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -24,4 +28,12 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)retrieve {
+    
+    [[NSUserDefaults standardUserDefaults] setObject:id_user.text forKey:@"pref_id"];
+    [[NSUserDefaults standardUserDefaults] setObject:pwd_user.text forKey:@"pref_pwd"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    //call user.authenticate
+    //navigate to tab view - tab view did load calls user.retrieveMails
+}
 @end
